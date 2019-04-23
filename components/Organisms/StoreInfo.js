35cc6fb12c014595a904fa97024535ad186fa6e7
floydkim <r2d2em@empal.com>
2019-04-23 15:42:08 +0900

    서버 API로 가게 상세정보 받아오도록 수정 (레이아웃 조정 필요)

--- components/Organisms/StoreInfo.js
+++ components/Organisms/StoreInfo.js
@@ -5,15 +5,21 @@ import MenusDisplay from '../Molecules/MenusDisplay';
 
 export default class StoreInfo extends Component {
   render() {
-    const { address, shopHours, dayOff, contact } = this.props.storeInfo;
+    const {
+      address,
+      openhour,
+      closehour,
+      dayoff,
+      contact
+    } = this.props.storeInfo;
     const { menuList } = this.props;
     return (
       <View style={{ borderWidth: 1 }}>
         <Text> Store Information (Organisms) </Text>
         <Text h4>{address}</Text>
-        <Text h4>
-          {shopHours} ({dayOff}요일 휴무)
-        </Text>
+        <Text h4>{openhour}</Text>
+        <Text h4>{closehour}</Text>
+        <Text h4>{dayoff}요일 휴무</Text>
         <Text h4>{contact}</Text>
         <MenusDisplay menuList={menuList} />
       </View>
