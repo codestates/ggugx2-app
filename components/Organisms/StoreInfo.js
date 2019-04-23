7e78b04a4d537dd75151eeee5e7e12535b4f03ec
floydkim <r2d2em@empal.com>
2019-04-23 16:47:49 +0900

    가게 상세정보 스타일 조정 및 전화걸기 링크 삽입

--- components/Organisms/StoreInfo.js
+++ components/Organisms/StoreInfo.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import { View, Linking, TouchableOpacity, Image } from 'react-native';
+import { Text, ThemeProvider } from 'react-native-elements';
 import MenusDisplay from '../Molecules/MenusDisplay';
 
+const theme = {
+  Text: {
+    style: {
+      height: 40
+    }
+  }
+};
 export default class StoreInfo extends Component {
   render() {
     const {
@@ -13,14 +20,39 @@ export default class StoreInfo extends Component {
       contact
     } = this.props.storeInfo;
     const { menuList } = this.props;
+    const iconOpen = require('../../assets/images/icon-open.png');
     return (
-      <View style={{ borderWidth: 1 }}>
-        <Text> Store Information (Organisms) </Text>
-        <Text h4>{address}</Text>
-        <Text h4>{openhour}</Text>
-        <Text h4>{closehour}</Text>
-        <Text h4>{dayoff}요일 휴무</Text>
-        <Text h4>{contact}</Text>
+      <View style={{ borderWidth: 0, padding: 6 }}>
+        <ThemeProvider theme={theme}>
+          <Text style={{ fontSize: 29 }}>{address}</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              borderWidth: 0,
+              alignItems: 'baseline'
+            }}
+          >
+            <Image
+              source={iconOpen}
+              style={{ width: 30, height: 30, margin: 4 }}
+            />
+            <Text style={{ fontSize: 28 }}>
+              {openhour ? openhour.slice(0, -3) : 'error'} ~{' '}
+              {closehour ? closehour.slice(0, -3) : 'error'}
+            </Text>
+          </View>
+          <Text style={{ fontSize: 24 }}>{dayoff}요일 휴무</Text>
+          <TouchableOpacity
+            onPress={() => {
+              Linking.openURL(`tel:${contact}`);
+            }}
+            style={{ borderWidth: 0, width: 200 }}
+          >
+            <Text h4 style={{ color: 'darkblue' }}>
+              {contact}
+            </Text>
+          </TouchableOpacity>
+        </ThemeProvider>
         <MenusDisplay menuList={menuList} />
       </View>
     );
