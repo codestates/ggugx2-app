76df51dcbf940db202147906539e060929aaded3
floydkim <r2d2em@empal.com>
2019-04-23 17:01:56 +0900

    가게 상세정보 스타일 조정

--- components/Organisms/StoreInfo.js
+++ components/Organisms/StoreInfo.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { View, Linking, TouchableOpacity, Image } from 'react-native';
+import {
+  View,
+  Linking,
+  TouchableOpacity,
+  Image,
+  StyleSheet
+} from 'react-native';
 import { Text, ThemeProvider } from 'react-native-elements';
 import MenusDisplay from '../Molecules/MenusDisplay';
 
@@ -10,6 +16,16 @@ const theme = {
     }
   }
 };
+
+const s = StyleSheet.create({
+  title: {
+    width: 80,
+    fontSize: 18,
+    paddingTop: 8,
+    fontWeight: 'bold'
+  }
+});
+
 export default class StoreInfo extends Component {
   render() {
     const {
@@ -22,36 +38,47 @@ export default class StoreInfo extends Component {
     const { menuList } = this.props;
     const iconOpen = require('../../assets/images/icon-open.png');
     return (
-      <View style={{ borderWidth: 0, padding: 6 }}>
+      <View style={{ borderWidth: 0, padding: 10 }}>
         <ThemeProvider theme={theme}>
-          <Text style={{ fontSize: 29 }}>{address}</Text>
-          <View
-            style={{
-              flexDirection: 'row',
-              borderWidth: 0,
-              alignItems: 'baseline'
-            }}
-          >
-            <Image
-              source={iconOpen}
-              style={{ width: 30, height: 30, margin: 4 }}
-            />
-            <Text style={{ fontSize: 28 }}>
-              {openhour ? openhour.slice(0, -3) : 'error'} ~{' '}
-              {closehour ? closehour.slice(0, -3) : 'error'}
-            </Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={s.title}>주소</Text>
+            <Text style={{ fontSize: 29 }}>{address}</Text>
           </View>
-          <Text style={{ fontSize: 24 }}>{dayoff}요일 휴무</Text>
-          <TouchableOpacity
-            onPress={() => {
-              Linking.openURL(`tel:${contact}`);
-            }}
-            style={{ borderWidth: 0, width: 200 }}
-          >
-            <Text h4 style={{ color: 'darkblue' }}>
-              {contact}
-            </Text>
-          </TouchableOpacity>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={s.title}>영업시간</Text>
+            <View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  borderWidth: 0,
+                  alignItems: 'baseline'
+                }}
+              >
+                <Image
+                  source={iconOpen}
+                  style={{ width: 30, height: 30, margin: 4 }}
+                />
+                <Text style={{ fontSize: 28 }}>
+                  {openhour ? openhour.slice(0, -3) : 'error'} ~{' '}
+                  {closehour ? closehour.slice(0, -3) : 'error'}
+                </Text>
+              </View>
+              <Text style={{ fontSize: 24 }}>{dayoff}요일 휴무</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={s.title}>전화번호</Text>
+            <TouchableOpacity
+              onPress={() => {
+                Linking.openURL(`tel:${contact}`);
+              }}
+              style={{ borderWidth: 0, width: 200 }}
+            >
+              <Text h4 style={{ color: 'darkblue' }}>
+                {contact}
+              </Text>
+            </TouchableOpacity>
+          </View>
         </ThemeProvider>
         <MenusDisplay menuList={menuList} />
       </View>
